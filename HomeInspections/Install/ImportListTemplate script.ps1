d324e3da4843cfa33941d0a4bba83637104c9971
MichelLaplane <michel.laplane@hotmail.com>
2022-05-18 22:36:35 +0200

    Added install files

---
+++ HomeInspections/Install/ImportListTemplate script.ps1
@@ -0,0 +1,8 @@
+﻿# Destination site collection url
+$url="https://yourtenant.sharepoint.com/sites/yoursite"
+# Connect to SharePoint Online site  
+Connect-PnPOnline -Url $Url
+
+Apply-PnPProvisioningTemplate -Path "yourpath\Home Inspections Template.xml"
+
+Write-host "Successfully a list has been created in SharePoint online."
